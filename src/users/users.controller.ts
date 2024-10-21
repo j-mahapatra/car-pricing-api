4b662f3134b44c89b32160f620915a3fe8dc43f2
j-mahapatra <jagannathm.official@gmail.com>
2024-10-21 07:49:22 +0530

    add CRUD operations for users module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    const { email, password } = body;
+    this.userService.create(email, password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    const parsedId = parseInt(id);
+    return this.userService.findOne(parsedId);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.find();
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    const parsedId = parseInt(id);
+    return this.userService.remove(parsedId);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    const parsedId = parseInt(id);
+    return this.userService.update(parsedId, body);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+    return this.repo.save(user);
+  }
+
+  findOne(id: number) {
+    const user = this.repo.findOneBy({ id });
+    return user;
+  }
+
+  find() {
+    const users = this.repo.find();
+    return users;
+  }
+
+  async update(id: number, dataToUpdate: Partial<User>) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    Object.assign(user, dataToUpdate);
+
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return this.repo.remove(user);
+  }
+}
